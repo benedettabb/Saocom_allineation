025bfa6962d60173fe81394b8eef4639cecf6bd1
Besz <80822451+benedettabb@users.noreply.github.com>
2025-09-19 14:58:54 +0200

    Update Cross_correlation_and_shift_v1.py

--- Cross_correlation_and_shift_v1.py
+++ Cross_correlation_and_shift_v1.py
@@ -3,21 +3,22 @@ import numpy as np
 import os
 from scipy.signal import fftconvolve
 import matplotlib.pyplot as plt
-
+from pathlib import Path
+import glob 
+from scipy.stats import pearsonr
+import sys
+from multiprocessing import Pool, cpu_count
+log_file = r"D:\Saocom_test\aligned\log.txt"
 
 def cross_fft(img1, img2, mask=None):
-    if mask is not None:
-        img1 = np.where(mask, img1, 0).astype(float)
-        img2 = np.where(mask, img2, 0).astype(float)
+    img1 = np.where(mask, img1, 0).astype(float)
+    img2 = np.where(mask, img2, 0).astype(float)
     img1 -= np.mean(img1)
     img2 -= np.mean(img2)
     corr = fftconvolve(img1, img2[::-1, ::-1], mode='full')
     y_max, x_max = np.unravel_index(np.argmax(corr), corr.shape)
     shift_y = y_max - (img1.shape[0] - 1)
     shift_x = x_max - (img1.shape[1] - 1)
-    print(shift_y, shift_x)
-    plt.imshow(corr)
-    plt.show()
     return shift_y, shift_x
 
 def apply_shift(img, shift, nodata=-9999):
@@ -35,43 +36,50 @@ def apply_shift(img, shift, nodata=-9999):
         img_shifted[:, dx:] = nodata
     return img_shifted
 
-def read(path):
+def read(path, nodata=-9999):
     with rasterio.open(path) as src:
         data = src.read()  # shape: (bands, rows, cols)
         profile = src.profile
         mask = data[0] != nodata  # usa prima banda per mask
     return profile, mask, data
 
-folder = r"D:\Saocom_test"
-asc_path = r"asc.tif"
-des_path = r"desc.tif"
-ref_path = r"S1_ref.tif"
-nodata = -9999
+def align(saocom, sentinel1, nodata = -9999):
+    out = os.path.join(r"D:\Saocom_test\aligned", Path(saocom).name)
+    if not os.path.isfile(out):
+        try:
+            profile_img, img_mask, img = read(saocom)
+            _, ref_mask, ref = read(sentinel1)
 
-# Read multiband
-profile_asc, asc_mask, asc = read(os.path.join(folder, asc_path))
-profile_des, des_mask, des = read(os.path.join(folder, des_path))
-profile_ref, ref_mask, ref = read(os.path.join(folder, ref_path))
+            # Calcola cross-corr
+            shift_y, shift_x = cross_fft(img[0], ref[0], mask=img_mask & ref_mask)
+            aligned = np.zeros_like(img)
+            for b in range(img.shape[0]):
+                aligned[b] = apply_shift(img[b], [shift_y, shift_x], nodata)
 
-# Calcola lo shift usando solo la prima banda
-shift_y_asc, shift_x_asc   = cross_fft(asc[0], ref[0], mask=asc_mask & ref_mask)
-shift_y_des, shift_x_des = cross_fft(des[0], ref[0], mask=des_mask & ref_mask)
+            # Guarda correlazione
+            valid_mask = (aligned[0] != -9999) & (ref[0] != -9999)
+            r_before = pearsonr(img[0][valid_mask].ravel(), ref[0][valid_mask].ravel())[0]
+            r_after = pearsonr(aligned[0][valid_mask].ravel(), ref[0][valid_mask].ravel())[0]
 
-# Applica lo shift a tutte le bande
-asc_aligned = np.zeros_like(asc)
-des_aligned = np.zeros_like(des)
-for b in range(asc.shape[0]):
-    asc_aligned[b] = apply_shift(asc[b], [shift_y_asc, shift_x_asc], nodata)
-for b in range(des.shape[0]):
-    des_aligned[b] = apply_shift(des[b], [shift_y_des, shift_x_des], nodata)
+            # Scrivi direttamente sul file
+            with open(log_file, "a") as f:
+                f.write(f"{saocom} {shift_y} {shift_x} {r_before:.3f} {r_after:.3f}\n")
 
-# Percorsi output
-asc_out = os.path.join(folder, "asc_aligned.tif")
-des_out = os.path.join(folder, "des_aligned.tif")
+            with rasterio.open(out, 'w', **profile_img) as dst:
+                dst.write(aligned)
+        except Exception as e:
+            print(e)
 
-# Salvataggio multibanda
-with rasterio.open(asc_out, 'w', **profile_asc) as dst:
-    dst.write(asc_aligned)
+if __name__ == '__main__':
+    asc_imgs = glob.glob(r"D:\EOVeg\DEFINITIVE\Data\*ASC_E051N016T1.tif")
+    des_imgs = glob.glob(r"D:\EOVeg\DEFINITIVE\Data\*DES_E051N016T1.tif")
+    asc_ref = r"D:\Saocom_test\Sentine1\asc\asc_ref_E051N016T1.tif"
+    des_ref = r"D:\Saocom_test\Sentine1\des\desc_ref_E051N016T1.tif"
+    # Crea lista di coppie (file, ref)
+    pairs = [(img, asc_ref) for img in asc_imgs] + [(img, des_ref) for img in des_imgs]
+    # Lancia multiprocessing
+    with Pool(2) as pool:
+        pool.starmap(align, pairs)
 
-with rasterio.open(des_out, 'w', **profile_des) as dst:
-    dst.write(des_aligned)
+sys.stdout.close()
+sys.stdout = sys.__stdout__
